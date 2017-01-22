8e299847e4026e104b8c9800765b1eb67e543eac
Zaid Khraibut <khraibut.z@gmail.com>
2017-01-22 03:51:33 -0500

    Added function "default" and "rainbow" buttons. Rainbow only turns line to blue

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,48 @@ var i = 0
 
 function createDivs() {
   while (i < (400*400)/100) {
-    $(".container").append('<div class="grid"></div>');
+    $(".grid").append('<div></div>');
     i++;
    }
 };
 
-createDivs();
-
-$(document).ready(function() {
-  $('.container').on('mouseover', 'div', function() {
+function greyGrid() {
+  $('.grid').on('mouseover', 'div', function() {
+    $(this).removeClass('blue');
     $(this).addClass('hovered');
   });
+};
+
+function rainbowGrid() {
+  $('.grid').on('mouseover', 'div', function() {
+    $(this).removeClass('hovered');
+    $(this).addClass('blue');
+  });
+};
+
+function clear() {
+  $('.grid>div').removeClass('hovered');
+  $('.grid>div').removeClass('blue');
+};
+
+$(document).ready(function() {
+  createDivs();
+  rainbowGrid();
+
   $('#clear').on('click', function() {
-    $('.hovered').toggleClass('hovered');
+    clear();
+  });
+
+  $('#default').on('click', function() {
+    clear();
+    greyGrid();
+  });
+
+  $('#rainbow').on('click', function() {
+    clear();
+    rainbowGrid();
   });
 
 });
-
-function emptyDivs() {
-  $('.container').empty();
-  createDivs();
-};
-
 
 // $('.container > div').toggleClass('hovered'); Use this to invert selection
