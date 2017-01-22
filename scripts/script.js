fce0a0203ef5ec6db9f65cd19c1c7fc6f31e89b8
Zaid Khraibut <khraibut.z@gmail.com>
2017-01-22 02:57:32 -0500

    Added styles, functional clear button, changed line color

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,28 @@
 var i = 0
 
-while (i < 16 * 16) {
-  $(".container").append('<div></div>');
-  i++;
- }
+function createDivs() {
+  while (i < (400*400)/100) {
+    $(".container").append('<div class="grid"></div>');
+    i++;
+   }
+};
+
+createDivs();
 
 $(document).ready(function() {
   $('.container').on('mouseover', 'div', function() {
-    $(this).css('background-color', 'black');
+    $(this).addClass('hovered');
   });
+  $('#clear').on('click', function() {
+    $('.hovered').toggleClass('hovered');
+  });
+
 });
+
+function emptyDivs() {
+  $('.container').empty();
+  createDivs();
+};
+
+
+// $('.container > div').toggleClass('hovered'); Use this to invert selection
